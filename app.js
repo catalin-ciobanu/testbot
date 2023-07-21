6f36f4908dbb7f7155b62da3a280fc1bff203737
Catalin Ciobanu <catalin.ciobanu@hindawi.com>
2023-07-21 12:20:23 +0300

    Logging only what's important

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.listen(myport, () => console.log("webhook is listening at port: " + myport))
 app.post("/webhook", (req, res) => {
   // Parse the request body from the POST
   let body = req.body;
-  console.log("Incoming request "+ new Date()+ JSON.stringify(req.body, null, 2));
+  
 
   // Check the Incoming webhook message
   //console.log(JSON.stringify(req.body, null, 2));
@@ -43,6 +43,7 @@ app.post("/webhook", (req, res) => {
       req.body.entry[0].changes[0].value.messages &&
       req.body.entry[0].changes[0].value.messages[0]
     ) {
+      console.log("Incoming request "+ new Date()+ JSON.stringify(req.body, null, 2));
       let phone_number_id =
         req.body.entry[0].changes[0].value.metadata.phone_number_id;
       let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
